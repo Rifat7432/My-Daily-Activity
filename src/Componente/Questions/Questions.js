7876c6aec8428fb94da406c634783a81071ae226
Rifat7432 <mdr341762@gmail.com>
2022-09-29 04:50:50 +0600

    finally finish

--- src/Componente/Questions/Questions.js
+++ src/Componente/Questions/Questions.js
@@ -14,7 +14,7 @@ const Questions = () => {
             <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components</p>
            </div>
            <div>
-            <h3>any other use of useeffect in react  without loading data </h3>
+            <h3>any other use of useeffect in react  without loading data ?</h3>
             <p>The motivation behind the introduction of useEffect Hook is to eliminate the side-effects of using class-based components. For example, tasks like updating the DOM, fetching data from API end-points, setting up subscriptions or timers, etc can be lead to unwarranted side-effects</p>
            </div>
         </div>
